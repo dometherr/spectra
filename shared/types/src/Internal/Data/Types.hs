ad92856a34385048711e324c0687d1fdb684d661
Guilherme Herter <nogr.gh@gmail.com>
2025-11-16 17:13:50 -0300

    Defining duration and setting format rules

--- shared/types/src/Internal/Data/Types.hs
+++ shared/types/src/Internal/Data/Types.hs
@@ -1,7 +1,12 @@
 module Internal.Data.Types (
     Timestamp,
     unTimestamp,
-    mkTimestamp
+    mkTimestamp, 
+
+    Duration,
+    unDuration,
+    mkDuration,
+    addd
 ) where
 
 -- | A timestamp represents a point in time
@@ -9,5 +14,19 @@ newtype Timestamp
     = Timestamp { unTimestamp :: Integer }
     deriving (Show, Eq)
 
+-- | A timestamp builder
 mkTimestamp :: Integer -> Timestamp
 mkTimestamp = Timestamp
+
+-- | A duration represents a period of time
+newtype Duration
+    = Duration { unDuration :: Integer }
+    deriving (Show, Eq)
+
+-- | A duration builder
+mkDuration :: Integer -> Duration
+mkDuration = Duration
+
+-- | A function that adds a Integer to a Duration
+addd :: Integer -> Duration -> Duration
+addd n d = mkDuration $ (unDuration d + n)
