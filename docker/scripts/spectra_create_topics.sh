70c39b8d40bc0862e5175fd5570bd295d6d2add8
Guilherme Herter <nogr.gh@gmail.com>
2025-11-17 22:32:14 -0300

    Adding symbol enriched topic and script cleanup

--- docker/scripts/spectra_create_topics.sh
+++ docker/scripts/spectra_create_topics.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Waiting for Kafka to become ready on spectra-broker:9092..."
+echo "Waiting readiness for Spectra Broker@19092..."
+
 while ! nc -z spectra-broker 19092; do
   sleep 1
 done
 
-echo "Creating spectra.engine.v1.lifecycle topic..."
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server spectra-broker:19092 --create --if-not-exists \
-  --topic spectra.engine.v1.lifecycle \
-  --partitions 3 \
-  --replication-factor 1
+echo "Spectra Broker is ready, proceeding with topics creation..."
 
-echo "Creating spectra.signal.v1.created topic..."
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server spectra-broker:19092 --create --if-not-exists \
-  --topic spectra.signal.v1.created \
-  --partitions 3 \
-  --replication-factor 1
+function create_topic() {
+  local topic="$1"
+  local partitions="$2"
+  local replication="$3"
 
-echo "Creating spectra.agent.v1.signal topic..."
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server spectra-broker:19092 --create --if-not-exists \
-  --topic spectra.agent.v1.signal \
-  --partitions 3 \
-  --replication-factor 1
+  echo "Checking topic: ${topic} readiness..."
+  /opt/kafka/bin/kafka-topics.sh \
+    --bootstrap-server spectra-broker:19092 \
+    --create \
+    --if-not-exists \
+    --topic "${topic}" \
+    --partitions "${partitions}" \
+    --replication-factor "${replication}"
+}
 
-echo "Create topics has finished"
+create_topic "spectra.agent.v1.signal" 3 1
+create_topic "spectra.engine.v1.lifecycle" 3 1
+create_topic "spectra.signal.v1.created" 3 1
+create_topic "spectra.symbol.v1.enriched" 3 1
+
+echo "Spectra topics are ready"
